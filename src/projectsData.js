5e4dc69a26b0dff7b7df80d6f6bd0bb41be212a5
Robert Roman <robert@192.168.1.131>
2024-09-20 11:05:42 +0300

    initial commit

---
+++ src/projectsData.js
@@ -0,0 +1,79 @@
+const projects = [
+  {
+    projectId: 1,
+    title: "Urbanlab",
+    description:
+      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+    coverImage:
+      "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfDB8MHx8fDA%3D",
+    projectImages: [
+      "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+    ],
+    technologies: ["HTML", "CSS", "JS", "Responsive Design", "Tailwind CSS"],
+    projectUrl: "www.urbanlab.com",
+    projectDate: "2022",
+  },
+  {
+    projectId: 2,
+    title: "Lazy Consulting",
+    description:
+      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+    coverImage:
+      "https://images.unsplash.com/photo-1726147593580-45f3f73e3781?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
+    projectImages: [
+      "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+    ],
+    technologies: ["HTML", "CSS", "JS", "Responsive Design"],
+    projectUrl: "www.urbanlab.com",
+    projectDate: "2022",
+  },
+  {
+    projectId: 3,
+    title: "Dopodoheal",
+    description:
+      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+    coverImage:
+      "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfDB8MHx8fDA%3D",
+    projectImages: [
+      "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+    ],
+    technologies: ["HTML", "CSS", "JS", "Responsive Design"],
+    projectUrl: "www.urbanlab.com",
+    projectDate: "2022",
+  },
+  {
+    projectId: 4,
+    title: "Kevin Rabichow",
+    description:
+      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+    coverImage:
+      "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfDB8MHx8fDA%3D",
+    projectImages: [
+      "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+    ],
+    technologies: ["HTML", "CSS", "JS", "Responsive Design", "Tailwind CSS"],
+    projectUrl: "www.urbanlab.com",
+    projectDate: "2022",
+  },
+  {
+    projectId: 5,
+    title: "Easy Bank",
+    description:
+      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+    coverImage:
+      "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfDB8MHx8fDA%3D",
+    projectImages: [
+      "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+    ],
+    technologies: ["HTML", "CSS", "JS"],
+    projectUrl: "www.urbanlab.com",
+    projectDate: "2022",
+  },
+];
+
+export default projects;
