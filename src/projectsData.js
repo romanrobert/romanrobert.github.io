7d146a10a6051c6b66e358bfd8cdb37d0303f335
Robert Roman <robert@192.168.1.131>
2024-09-25 10:01:30 +0300

    projects update

--- src/projectsData.js
+++ src/projectsData.js
@@ -3,58 +3,58 @@ const projects = [
     projectId: 1,
     title: "Urbanlab",
     description:
-      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
-    coverImage: "/assets/urbanlab-img-main.png",
+      "As the frontend developer for urbanlab.ro, I created a responsive website that showcases Urbanlab's mission. I also developed a landing page for the Ecolab project and implemented a user-friendly content management system to facilitate easy maintenance of the site. This project demonstrated my ability to build visually appealing and functional web interfaces, provided by Qubits Design Studio while ensuring straightforward content management for the Urbanlab team.",
+    coverImage: "/assets/urbanlab/urbanlab-cover-image.jpg",
     projectImages: [
       "/assets/urbanlab-img-project.png",
-      "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+      "/assets/urbanlab/urbanlab-portfolio-img-1.jpg",
     ],
-    technologies: ["HTML", "CSS", "JS", "Responsive Design", "Tailwind CSS"],
-    projectUrl: "www.urbanlab.com",
-    projectDate: "2022",
+    technologies: ["HTML", "CSS", "JS", "Responsive Design", "Webflow"],
+    projectUrl: "https://www.urbanlab.ro",
+    projectDate: "2024",
   },
   {
     projectId: 2,
     title: "Lazy Consulting",
     description:
-      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+      "As the frontend developer for Lazy Consulting's AI webinar project, I designed and implemented a sales funnel landing page. My focus was on creating a user-friendly and responsive interface that effectively attracts and captures new leads. This project showcased my ability to develop engaging web experiences that drive user conversion and support business growth through targeted digital marketing strategies.",
     coverImage:
       "https://images.unsplash.com/photo-1726147593580-45f3f73e3781?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
     projectImages: [
       "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
       "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
     ],
-    technologies: ["HTML", "CSS", "JS", "Responsive Design"],
-    projectUrl: "www.urbanlab.com",
-    projectDate: "2022",
+    technologies: ["HTML", "CSS", "JS", "Responsive Design", "Webflow"],
+    projectUrl: "https://www.lazyconsulting.com/",
+    projectDate: "2023",
   },
   {
     projectId: 3,
     title: "Dopodoheal",
     description:
-      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+      "For Dopodoheal.com, I collaborated with Qubits Design Studio to create a comprehensive website for their podiatry services. The project involved developing an informative platform that not only showcases their services but also educates potential clients. I implemented a user-friendly booking system to streamline appointment scheduling and integrated a small e-commerce shop for product sales.",
     coverImage:
       "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfDB8MHx8fDA%3D",
     projectImages: [
       "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
       "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
     ],
-    technologies: ["HTML", "CSS", "JS", "Responsive Design"],
-    projectUrl: "www.urbanlab.com",
-    projectDate: "2022",
+    technologies: ["HTML", "CSS", "JS", "Webflow", "CMS", "Ecommerce"],
+    projectUrl: "https://www.dopodoheal.com",
+    projectDate: "2023",
   },
   {
     projectId: 4,
     title: "Kevin Rabichow",
     description:
-      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+      "I was responsible for the overhaul of Kevin Rabichow's website, aiming to create a user-friendly platform for individuals to gain insights into his poker coaching service and methodologies.",
     coverImage:
       "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfDB8MHx8fDA%3D",
     projectImages: [
       "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
       "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
     ],
-    technologies: ["HTML", "CSS", "JS", "Responsive Design", "Tailwind CSS"],
+    technologies: ["Wix", "CMS", "Responsive Design", "Figma"],
     projectUrl: "www.urbanlab.com",
     projectDate: "2022",
   },
@@ -62,14 +62,14 @@ const projects = [
     projectId: 5,
     title: "Easy Bank",
     description:
-      "Urbanlab is a platform that connects urban designers and developers with each other and with the public. We are building a platform that allows urban designers to create and share their designs, and urban developers to use them in their projects.",
+      "The Easybank project is one of several challenges I completed on Frontend Mentor to hone my development skills. This project, along with others in my portfolio, demonstrates my commitment to continuous improvement and practical application of frontend techniques. By participating in these challenges, I've strengthened my ability to turn design concepts into functional, responsive web interfaces. My Frontend Mentor profile showcases a range of projects that reflect my growing expertise in frontend development.",
     coverImage:
       "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfDB8MHx8fDA%3D",
     projectImages: [
       "https://images.unsplash.com/photo-1597218041286-e8b6048d48c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
       "https://plus.unsplash.com/premium_photo-1664391952278-9a0be5649abb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVoY25vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
     ],
-    technologies: ["HTML", "CSS", "JS"],
+    technologies: ["HTML", "CSS", "JS", "Responsive", "Mobile First Aproach"],
     projectUrl: "www.urbanlab.com",
     projectDate: "2022",
   },
